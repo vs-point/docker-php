e7fe011eeb030fe843af53e37861129b0e631ba9
Vít Steklý <stekly.vit@vs-point.cz>
2021-01-19 19:21:53 +0100

    update bash

--- build.sh
+++ build.sh
@@ -8,6 +8,25 @@ docker push vspoint/php:5.6-fpm-alpine
 docker build --no-cache -t vspoint/php:5.6-fpm-alpine-production php/5.6-fpm-alpine-production/.
 docker push vspoint/php:5.6-fpm-alpine-production
 
+
+docker image rm php:7.1-fpm-alpine
+docker build --no-cache -t vspoint/php:7.1-fpm-alpine php/7.1-fpm-alpine/.
+docker push vspoint/php:7.1-fpm-alpine
+
+docker image rm php:7.2-fpm-alpine
+docker build --no-cache -t vspoint/php:7.2-fpm-alpine php/7.2-fpm-alpine/.
+docker push vspoint/php:7.2-fpm-alpine
+
+
+docker image rm php:7.3-fpm-alpine
+
+docker build --no-cache -t vspoint/php:7.3-fpm-alpine php/7.3-fpm-alpine/.
+docker push vspoint/php:7.3-fpm-alpine
+
+docker build --no-cache -t vspoint/php:7.3-fpm-alpine-production php/7.3-fpm-alpine-production/.
+docker push vspoint/php:7.3-fpm-alpine-production
+
+
 docker image rm php:7.4-fpm-alpine
 
 docker build --no-cache -t vspoint/php:7.4-fpm-alpine php/7.4-fpm-alpine/.
@@ -15,6 +34,7 @@ docker push vspoint/php:7.4-fpm-alpine
 
 docker build --no-cache -t vspoint/php:7.4-fpm-alpine-production php/7.4-fpm-alpine-production/.
 docker push vspoint/php:7.4-fpm-alpine-production
+
 
 docker image rm php:8.0-fpm-alpine
 
